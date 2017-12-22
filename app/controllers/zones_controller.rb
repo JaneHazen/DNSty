269264eb36e9478c9689cd1cb46e764f852b8337
Jane <iamjane.gmail.com>
2017-12-21 18:38:55 -0600

    List active zones from nsone api on zones index page

---
+++ app/controllers/zones_controller.rb
@@ -0,0 +1,114 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class ZonesController < ApplicationController
+  before_action :set_zone, only: [:show, :edit, :update, :destroy]
+
+  # GET /zones
+  # GET /zones.json
+  def index
+    @json = JSON(get_active_nsone_zones.body)
+    @zones = Zone.all
+
+  end
+
+  # GET /zones/1
+  # GET /zones/1.json
+  def show
+  end
+
+  # GET /zones/new
+  def new
+    @zone = Zone.new
+  end
+
+  # GET /zones/1/edit
+  def edit
+  end
+
+  # POST /zones
+  # POST /zones.json
+  def create
+    p "*"
+    p params
+    @zone = Zone.new(zone: params["zone"])
+    # p "$" * 100
+    create_nsone_zone(params["zone"])
+    p "%" * 100
+
+    respond_to do |format|
+      if @zone.save
+        format.html { redirect_to @zone, notice: 'Zone was successfully created.' }
+        format.json { render :show, status: :created, location: @zone }
+      else
+        format.html { render :new }
+        format.json { render json: @zone.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /zones/1
+  # PATCH/PUT /zones/1.json
+  def update
+    respond_to do |format|
+      if @zone.update(zone_params)
+        format.html { redirect_to @zone, notice: 'Zone was successfully updated.' }
+        format.json { render :show, status: :ok, location: @zone }
+      else
+        format.html { render :edit }
+        format.json { render json: @zone.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /zones/1
+  # DELETE /zones/1.json
+  def destroy
+    @zone.destroy
+    respond_to do |format|
+      format.html { redirect_to zones_url, notice: 'Zone was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_zone
+      @zone = Zone.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def create_nsone_zone(new_zone)
+      p "$" * 100
+      uri = URI.parse("https://api.nsone.net/v1/zones/newzone.com")
+      request = Net::HTTP::Put.new(uri)
+      request["X-Nsone-Key"] = "#{ENV['API_KEY']}"
+      request.body = JSON.dump({
+      "zone" => "#{new_zone}",
+      "nx_ttl" => 60
+      })
+
+      req_options = {
+      use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+    end
+
+    def get_active_nsone_zones
+      uri = URI.parse("https://api.nsone.net/v1/zones")
+      request = Net::HTTP::Get.new(uri)
+      request["X-Nsone-Key"] = "#{ENV['API_KEY']}"
+
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+    end
+end
